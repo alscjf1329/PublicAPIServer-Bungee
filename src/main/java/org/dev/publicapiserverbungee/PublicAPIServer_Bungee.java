4282020fc25807b7f5c0004db3cff7da5a0ba85b
alscjf1329 <alscjf1329@gmail.com>
2024-01-28 16:30:38 +0900

    feat) 재시작 명령어 등록

--- src/main/java/org/dev/publicapiserverbungee/PublicAPIServer_Bungee.java
+++ src/main/java/org/dev/publicapiserverbungee/PublicAPIServer_Bungee.java
@@ -1,7 +1,9 @@
 package org.dev.publicapiserverbungee;
 
 import lombok.Getter;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
+import org.dev.publicapiserverbungee.command.ReloadCommand;
 import org.dev.publicapiserverbungee.server.PublicServer;
 
 public final class PublicAPIServer_Bungee extends Plugin {
@@ -14,6 +16,7 @@ public final class PublicAPIServer_Bungee extends Plugin {
         // Plugin startup logic
         pluginInstance = this;
         new Thread(() -> PublicServer.getInstance(this).start()).start();
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new ReloadCommand());
     }
 
     @Override
